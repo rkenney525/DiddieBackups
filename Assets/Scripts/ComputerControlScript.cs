7415f02aad65a941cafad5f08c35506a9fea75a4
Ryan Kenney <rkenney525@gmail.com>
2016-07-02 14:48:37 -0400

    Added stubs for scripts

---
+++ Assets/Scripts/ComputerControlScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Responsible for moving the computer instance, storing info, and handling being hacked
+/// </summary>
+public class ComputerControlScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
